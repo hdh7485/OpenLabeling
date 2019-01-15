738264bc44be63b9784ef63d220e47368b3a5fea
Donghee Han <hdh7485@gmail.com>
2019-01-15 13:43:47 +0900

    Modify

--- crawler.py
+++ crawler.py
@@ -3,6 +3,7 @@ from icrawler.builtin import GoogleImageCrawler
 def google_image_crawl(directory='lena', keyword='lena', max_num=500):
     google_crawler = GoogleImageCrawler(parser_threads=2, downloader_threads=4,
                                         storage={'root_dir': directory})
-    google_crawler.crawl(keyword=keyword, max_num=max_num, min_size=(200,200), max_size=None)
+    google_crawler.crawl(keyword=keyword, max_num=max_num, min_size=(200,200), max_size=None, file_idx_offset='auto')
 
-google_image_crawl(directory='images', keyword='blue signs', max_num=400)
+for keyword in ['red signs', 'blue signs']:
+    google_image_crawl(directory='images', keyword=keyword, max_num=1000)
--- make_list.py
+++ make_list.py
@@ -1,4 +1,5 @@
 import glob
-files = glob.glob('/home/hdh7485/darknet/darknet181102_tiny_negative/data/img/*.jpg')
+files = glob.glob('/home/hdh7485/darknet/darknet181209_tiny_face/data/img/*.jpg')
+files = files + glob.glob('/home/hdh7485/darknet/darknet181209_tiny_face/data/img/*.JPG')
 with open('train.txt', 'w') as f:
     f.writelines(fn+'\n' for fn in files)
